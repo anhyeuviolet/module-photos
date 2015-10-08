e3a20edfadcd7f905be00f69f71c607e43559b0e
Kenny Nguyen <nguyentiendat713@gmail.com>
2015-10-08 09:55:35 +0700

    Tag 1.3.06

--- modules/photos/version.php
+++ modules/photos/version.php
@@ -17,8 +17,8 @@ $module_version = array(
 	'submenu' => 'rss,search',
 	'is_sysmod' => 0, 
 	'virtual' => 1,  
-	'version' => '1.3.04',  
-	'date' => 'Mon, 28 Sep 2015 11:18:59 GMT',  
+	'version' => '1.3.06',  
+	'date' => 'Mon, 07 Oct 2015 11:18:59 GMT',  
 	'author' => 'KENNYNGUYEN (nguyentiendat713@gmail.com)',  
 	'uploads_dir' => array(
 		$module_upload,
