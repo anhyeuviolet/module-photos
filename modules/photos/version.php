3e39ab11f146e1b361c0596c116c0a4edbe162b5
Kenny Nguyen <nguyentiendat713@gmail.com>
2022-10-20 22:28:29 +0700

    Bump version to 1.4.05; Tested with NukeViet 4.5;
    Vietnamese women's day!

--- modules/photos/version.php
+++ modules/photos/version.php
@@ -6,6 +6,7 @@
  * @Based on NukeViet CMS
  * @License GNU/GPL version 2 or any later version
  * @Createdate Wed, 17 April 2019 08:00:00 GMT
+ * @ Thanks to Sebastian Tschan for BlueIMP gallery
  */
 
 if( !defined( 'NV_MAINFILE' ) )
@@ -17,8 +18,8 @@ $module_version = array(
 	'submenu' => 'rss,search',
 	'is_sysmod' => 0,
 	'virtual' => 1,
-	'version' => '1.4.04',
-	'date' => 'Wed, 17 April 2019 08:00:00 GMT',
+	'version' => '1.4.05',
+	'date' => 'Thu, 20 Oct 2022 08:00:00 GMT',
 	'author' => 'KENNY NGUYEN (nguyentiendat713@gmail.com)',
 	'uploads_dir' => array(
 		$module_upload,
